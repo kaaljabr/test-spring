44adbcdd0e2c9cc2897c502532478f3d3bbd0260
Kareem Aljabr <kareem.aljabr@universalorlando.com>
2021-04-25 14:03:56 -0400

    initial commit

---
+++ src/test/java/com/test/spring/testspring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.spring.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
